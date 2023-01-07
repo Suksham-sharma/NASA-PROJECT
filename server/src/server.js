57895ad0fe8bd74f90ce645a67fd4ae592c3bf5f
{Suksham-sharma} <sukshamever@gmail.com>
2023-01-07 18:52:01 +0530

    Connected the Server with Mongo DB , And created the schema for collections in Mongo Atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,30 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 const { LoadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:WmyhohODBbREpPrx@nasacluster.nnpuvru.mongodb.net/NASA?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: true,
+  });
   await LoadPlanetsData();
   const server = http.createServer(app);
   server.listen(PORT, () => {
